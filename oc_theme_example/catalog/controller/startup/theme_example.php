a6cc9a2b5e7373bd0bc64d2527d9d3a8a5b2d4a4
Daniel Kerr <webmaster@opencart.com>
2022-06-17 01:10:51 +0800

    theme example

---
+++ oc_theme_example/catalog/controller/startup/theme_example.php
@@ -0,0 +1,19 @@
+<?php
+namespace Opencart\Catalog\Controller\Extension\OcThemeExample\Startup;
+class ThemeExample extends \Opencart\System\Engine\Controller {
+	public function index(): void {
+		if ($this->config->get('theme_theme_example_status')) {
+			$this->event->register('view/extension/oc_theme_example/theme/theme_example/before', new Action('startup/theme_example|event'));
+		}
+	}
+
+	public function event(string &$route, array &$args, mixed &$output): void {
+		$override = [
+			'common/header'
+		];
+echo 'hi';
+		if (in_array($route, $override)) {
+			$route = 'extension/oc_theme_example/' . $route;
+		}
+	}
+}
