962875d756b3559f5a31a4213fad5915220c766b
Mohammad Ali Nekouie <mohammadalinekouie@gmail.com>
2022-09-30 16:27:32 +0330

    first edition of Farsi language module for opencart 4.0.1.1 based on examples

---
+++ admin/language/fa-fa/fa-fa.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @version		$Id: fa-fa.php 0001 2022-09-30 
+ * @package		Translation - Frontend
+ * @author		Mohammad Ali Nekouie
+ * @copyright	2022
+ * @license		GPL - www.gnu.org/copyleft/gpl.html
+ */
+
+// Locale
+$_['code']					= 'Fa';
+$_['direction']				= 'rtl';
+$_['date_format_short']		= 'd.m.Y';
+$_['date_format_long']		= 'l j. F Y';
+$_['time_format']			= 'H:i:s';
+$_['datetime_format']		= 'd.m.Y H:i:s';
+$_['decimal_point']			= ',';
+$_['thousand_point']		= '.';
+
+// Text
+$_['text_home']				= '<i class="fa fa-home"></i>';
+$_['text_yes']				= 'بله';
+$_['text_no']				= 'خیر';
+$_['text_none']				= ' -- هیچکدام -- ';
+$_['text_select']			= ' -- انتخاب کنید -- ';
+$_['text_all_zones']		= 'تمامی قسمت ها';
+$_['text_pagination']		= 'نمایش %d تا %d از %d (%d صفحه(ص))';
+$_['text_loading']			= 'بارگذاری';
+$_['text_no_results']		= 'چیزی یافت نشد';
+
+// Buttons
+$_['button_add_address']	= 'افزودن آدرس';
+$_['button_back']			= 'بازگشت';
+$_['button_continue']		= 'ادامه';
+$_['button_cart']			= 'سبد خرید';
+$_['button_cancel']			= 'انصراف';
+$_['button_compare']        = 'مقایسه کنید';
+$_['button_wishlist']       = 'علاقمندی ها';
+$_['button_checkout']       = 'تسویه حساب';
+$_['button_confirm']        = 'تایید';
+$_['button_coupon']         = 'کوپن نخفیف';
+$_['button_delete']         = 'حذف';
+$_['button_download']       = 'دانلود';
+$_['button_edit']           = 'ویرایش';
+$_['button_filter']         = 'فیلتر';
+$_['button_new_address']    = 'آدرس جدید';
+$_['button_change_address'] = 'تغییر آدرس';
+$_['button_reviews']        = 'نظرات';
+$_['button_write']          = 'نوشتن';
+$_['button_login']          = 'ورود';
+$_['button_update']         = 'تغییر';
+$_['button_remove']         = 'حذف';
+$_['button_reorder']        = 'مرتب سازی';
+$_['button_return']         = 'مرجوعی';
+$_['button_shopping']       = 'خرید';
+$_['button_search']         = 'جستجو';
+$_['button_shipping']       = 'حمل';
+$_['button_submit']			= 'انجام شود';
+$_['button_guest']          = 'میهمان';
+$_['button_view']           = 'نمایش';
+$_['button_voucher']        = 'از کوپن هدیه استفاده کنید';
+$_['button_upload']         = 'بارگذاری';
+$_['button_reward']         = 'امتیاز دهید';
+$_['button_quote']          = 'محاسبه هزینه حمل و نقل';
+$_['button_list']			= 'لیست';
+$_['button_grid']			= 'موزایکی';
+$_['button_map']            = 'نمایش نقشه';
+
+// Error
+$_['error_exception']       = 'کد خطا (%s): %s در خط %s %s';
+$_['error_upload_1']        = '(1) فایل بیش از اندازه فایل مجاز است!';
+$_['error_upload_2']        = '(2) فایل بیش از اندازه فایل مجاز است!';
+$_['error_upload_3']        = 'فایل فقط تا حدی آپلود شد!';
+$_['error_upload_4']        = 'هیچ فایلی آپلود نشد!';
+$_['error_upload_6']        = 'خطا در دایرکتوری موقت!';
+$_['error_upload_7']        = 'خطا در نوشتن فایل!';
+$_['error_upload_8']        = 'فرآیند بارگذاری به دلیل وقفه متوقف شد!';
+$_['error_upload_999']      = 'خطای نامشخص!';
+$_['error_curl']			= 'CURL: خطای #(%s): %s';
+
+// Summernote Editor 
+$_['summernote']			= 'fa-FA';
+// Datepicker 
+$_['datepicker']			= 'fa';
+
